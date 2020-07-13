94e7b6cbc89992c48b64a6b8ecf76cffbacbe7c1
Bator Zhigzhitov <b.zhigzhitov@unitpay.ru>
2020-07-13 20:01:43 +0300

    Добавление формы для комментов.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,15 +24,21 @@ class ConferenceController extends AbstractController
      * @var Environment
      */
     private $twig;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
     /**
      * ConferenceController constructor.
      *
-     * @param Environment $twig
+     * @param Environment            $twig
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -54,22 +64,49 @@ class ConferenceController extends AbstractController
      * @param Request           $request
      * @param Conference        $conference
      * @param CommentRepository $commentRepository
+     * @param string            $photoDir
      *
      * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $exception) {
+                    // unable to upload photo, give up
+                }
+
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
-            'conference' => $conference,
-            'comments'   => $paginator,
-            'previous'   => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next'       => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'conference'  => $conference,
+            'comments'    => $paginator,
+            'previous'    => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next'        => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'commentForm' => $form->createView(),
         ]));
     }
 }
